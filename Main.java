ac1fa289968d2f8886e291179eb29cc4c0a24095
benjamin.oluyomi <boluyomi@academytrainee.com>
2020-11-05 10:00:59 +0000

    added java file

---
+++ Main.java
@@ -0,0 +1,16 @@
+package proj1;
+
+public class Main {
+	public static void addition() {
+		int a = 1;
+		int b =2;
+		
+		int result = a + b;
+		System.out.println(result);
+	}
+	public static void main(String[] args) {
+		addition();
+		
+		
+}
+}
